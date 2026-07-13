003cb1529852b22b9701586697cec1a76f2dece5
mikh-maksi <newlevelcenter@gmail.com>
2026-07-13 17:33:52 +0300

    base

---
+++ base/base.php
@@ -0,0 +1,23 @@
+<?php
+$a = 1;
+$b = 2;
+$c = $a + $b;
+
+echo $c;
+
+
+$x = rand(1,10);
+
+echo $x;
+
+if ($x<5){
+    echo "x<5";
+}else{
+    echo "x>=5";
+}
+
+for($i=0;$i<10;$i++){
+    echo $i."<br>";
+}
+
+?> 
