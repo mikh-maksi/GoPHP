79543f0a0c6a087eff448075777fdd8860d432bf
mikh-maksi <newlevelcenter@gmail.com>
2026-07-27 18:20:22 +0300

    curl_map

--- curl/index_map.php
+++ curl/index_map.php
@@ -1,21 +1,31 @@
 <?php
- 
-   $url = "https://nominatim.openstreetmap.org/search?q=Khreshchatyk+1,+Kyiv&format=jsonv2";
 
-   //setting the curl parameters.
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);   
-  
-  $data = curl_exec($ch);
+$address = urlencode("Khreshchatyk 1, Kyiv");
+
+$url = "https://nominatim.openstreetmap.org/search?q={$address}&format=jsonv2";
+
+$options = [
+    "http" => [
+        "header" => "User-Agent: MyApplication/1.0\r\n"
+    ]
+];
+
+$context = stream_context_create($options);
+
+$response = file_get_contents($url, false, $context);
+
+$data = json_decode($response, true);
+
+if (!empty($data)) {
+    echo "Latitude: " . $data[0]['lat'] . PHP_EOL;
+    echo "Longitude: " . $data[0]['lon'] . PHP_EOL;
+}
 
 
-  curl_close($ch);
 
-  $array_data = json_decode($data);
+//   header('Access-Control-Allow-Origin: *');
 
-  header('Access-Control-Allow-Origin: *');
-
-// генерируем результат
-header('Content-type: application/json');
-echo json_encode($array_data); //отримуємо json объект
+// // генерируем результат
+// header('Content-type: application/json');
+// echo json_encode($array_data); //отримуємо json объект
 ?>
